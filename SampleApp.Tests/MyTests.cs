422b71ae5363b8417304a1f7e18995c2ff892dfd
theRookie <rookie0jnr@gmail.com>
2019-10-08 05:21:59 -0700

    Step 1. Testing framework removed completely!

--- SampleApp.Tests/MyTests.cs
+++ SampleApp.Tests/MyTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Components.Testing;
+﻿//using Microsoft.AspNetCore.Components.Testing;
 using SampleApp.Pages;
 using System;
 using Xunit;
@@ -7,22 +7,22 @@ namespace SampleApp.Tests
 {
     public class MyTests
     {
-        private TestHost host = new TestHost();
+        //private TestHost host = new TestHost();
 
         [Fact]
         public void CounterWorks()
         {
-            var component = host.AddComponent<Counter>();
-            Func<string> countValue = () => component.Find("#count").InnerText;
+            //var component = host.AddComponent<Counter>();
+            //Func<string> countValue = () => component.Find("#count").InnerText;
 
-            Assert.Equal("Counter", component.Find("h1").InnerText);
-            Assert.Equal("Current count: 0", countValue());
+            //Assert.Equal("Counter", component.Find("h1").InnerText);
+            //Assert.Equal("Current count: 0", countValue());
 
-            component.Find("button.inc").Click();
-            Assert.Contains("Current count: 1", countValue());
+            //component.Find("button.inc").Click();
+            //Assert.Contains("Current count: 1", countValue());
 
-            component.Find("button.dec").Click();
-            Assert.Contains("Current count: 0", countValue());
+            //component.Find("button.dec").Click();
+            //Assert.Contains("Current count: 0", countValue());
         }
     }
 }
